31b20f357246e95d96c1f66fba2cebef9dd0eb79
Виталий <x_15@mail.ru>
2021-03-10 23:19:10 +0200

    base

---
+++ lib/class-wp-json-booking.php
@@ -0,0 +1,196 @@
+<?php
+
+class WP_JSON_Booking {
+	/**
+	 * Server object
+	 *
+	 * @var WP_JSON_ResponseHandler
+	 */
+	protected $server;
+
+	/**
+	 * Constructor
+	 *
+	 * @param WP_JSON_ResponseHandler $server Server object
+	 */
+	public function __construct(WP_JSON_ResponseHandler $server) {
+		$this->server = $server;
+	}
+
+	/**
+	 * Register  routes
+	 *
+	 * @param array $routes Existing routes
+	 * @return array Modified routes
+	 */
+	public function register_routes( $routes ) {
+		$post_routes = array(
+			// Endpoints
+			'/booking/pirates' => array(
+                array( array( $this, 'get_timetable' ),  WP_JSON_Server::READABLE ),
+				array( array( $this, 'create_booking_pirates' ),    WP_JSON_Server::CREATABLE | WP_JSON_Server::ACCEPT_JSON ),
+			),
+            '/booking/maze' => array(
+                array( array( $this, 'get_timetable' ),  WP_JSON_Server::READABLE ),
+                array( array( $this, 'create_booking_maze' ),    WP_JSON_Server::CREATABLE | WP_JSON_Server::ACCEPT_JSON ),
+            ),
+            '/booking/mission' => array(
+                array( array( $this, 'get_timetable' ),  WP_JSON_Server::READABLE ),
+                array( array( $this, 'create_booking_mission' ),    WP_JSON_Server::CREATABLE | WP_JSON_Server::ACCEPT_JSON ),
+            ),
+            '/booking/aztec' => array(
+                array( array( $this, 'get_timetable' ),  WP_JSON_Server::READABLE ),
+                array( array( $this, 'create_booking_aztec' ),    WP_JSON_Server::CREATABLE | WP_JSON_Server::ACCEPT_JSON ),
+            )
+		);
+
+		return array_merge( $routes, $post_routes );
+	}
+
+	/**
+	 * @return stdClass[] Collection of Post entities
+	 */
+	public function get_timetable( $filter = array(), $context = 'view', $type = 'post', $page = 1 ) {
+
+        $response = array(
+            "USE POST INSTEAD OF GET"
+        );
+
+		return $response;
+	}
+
+    public function create_booking_pirates($data)
+    {
+        // Пираты Карибского моря: Сундук мертвеца
+        $post_id_regs = 492;
+        $id_calendars = 4;
+
+	    return $this->create_booking($data, $post_id_regs, $id_calendars);
+    }
+
+    public function create_booking_maze($data)
+    {
+        // Лабиринт
+        $post_id_regs = 495;
+        $id_calendars = 8;
+
+        return $this->create_booking($data, $post_id_regs, $id_calendars);
+    }
+
+    public function create_booking_mission($data)
+    {
+        // Миссия выполнима 2
+        $post_id_regs = 494;
+        $id_calendars = 6;
+
+        return $this->create_booking($data, $post_id_regs, $id_calendars);
+    }
+
+    public function create_booking_aztec($data)
+    {
+        // Сокровища ацтеков
+        $post_id_regs = 1770;
+        $id_calendars = 7;
+
+        return $this->create_booking($data, $post_id_regs, $id_calendars);
+    }
+
+	/**
+	 * @return array
+	 */
+	public function create_booking($data, $post_id_regs, $id_calendars) {
+
+	    $error = false;
+	    $error_msg = '[ошибка]';
+
+        if (empty($data['first_name']) && empty($data['family_name'])) {
+	        $error = true;
+            $error_msg .= '[не указали имя]';
+        }
+	    if (empty($data['phone']) && empty($data['email'])) {
+	        $error = true;
+            $error_msg .= '[не предоставили контактные данные]';
+        }
+        if (empty($data['date']) || empty($data['time'])) {
+            $error = true;
+            $error_msg .= '[не указали дату/время]';
+        } else {
+            // Booking ability check
+            $is_free = $this->check_busy_times($data['date'], $data['time'], $post_id_regs, $id_calendars) || false;
+
+            if (!$is_free) {
+                $error = true;
+                $error_msg = 'Указанное время занято';
+            }
+        }
+
+        if ($error) {
+            $response = array(
+                "success" => false,
+                "message" => $error_msg
+            );
+        } else {
+            $name = $data['first_name']." ".$data['family_name'];
+            $phone = $data['phone'];
+            $email = $data['email'];
+            $comment = $data['source']." | unique_id=".$data['unique_id']." | price=".$data['price']." | ".$data['comment'];
+            $time_to_book = $data['date']." ".$data['time'].":00";
+
+            $query_result = $this->insert_into_db($name, $phone, $email, $comment, $time_to_book, $post_id_regs, $id_calendars);
+
+            if ($query_result) {
+                $response = array(
+                    "success" => true,
+                );
+            } else {
+                $response = array(
+                    "success" => false,
+                    "message" => "не удалось сохранить"
+                );
+            }
+        }
+
+        return $response;
+	}
+
+    /**
+     * @return bool
+     */
+    public function insert_into_db($name, $phone, $email, $comment, $time_to_book, $post_id_regs, $id_calendars)
+    {
+        global $wpdb;
+        $now = date('Y-m-d H:i:s');
+        $query = "INSERT INTO regs(`cal_id`, `post_id`, `datt`, `nam`, `phone`, `eml`, `kogda`, `comment`, `stat`) VALUES (%d, %d, '%s', '%s', '%s', '%s', '%s', '%s', 0)";
+
+        $query  = $wpdb->prepare($query, array($id_calendars, $post_id_regs, $time_to_book, $name, $phone, $email, $now, $comment));
+        $success = $wpdb->query($query);
+        $wpdb->close();
+
+        return $success;
+    }
+
+    /**
+     * @return bool
+     */
+    public function check_busy_times($date_request, $time_request, $post_id_regs, $id_calendars)
+    {
+        global $wpdb;
+        $is_free = true;
+
+        $response = $wpdb->get_results( "SELECT datt FROM regs WHERE post_id=".$post_id_regs." ORDER BY id DESC LIMIT 20", ARRAY_A );
+
+        foreach ($response as $item) {
+
+            $date_arr = explode(" ", $item['datt']);
+            $date = $date_arr[0];
+            $time = $date_arr[1];
+            $time = substr($time, 0, 5);
+
+            if ($date_request == $date && $time_request == $time) $is_free = false;
+            //echo $date." ".$time."\n";
+        }
+
+        return $is_free;
+    }
+
+}
---
+++ lib/class-wp-json-timetable.php
@@ -0,0 +1,273 @@
+<?php
+
+class WP_JSON_Timetable {
+	/**
+	 * Server object
+	 *
+	 * @var WP_JSON_ResponseHandler
+	 */
+	protected $server;
+
+	/**
+	 * Constructor
+	 *
+	 * @param WP_JSON_ResponseHandler $server Server object
+	 */
+	public function __construct(WP_JSON_ResponseHandler $server) {
+		$this->server = $server;
+	}
+
+	/**
+	 * Register routes
+	 *
+	 * @param array $routes Existing routes
+	 * @return array Modified routes
+	 */
+	public function register_routes( $routes ) {
+		$post_routes = array(
+			// Quest timetables endpoints
+			'/timetable/pirates' => array(
+                array( array( $this, 'get_timetable_pirates' ),  WP_JSON_Server::READABLE )
+			),
+            '/timetable/maze' => array(
+                array( array( $this, 'get_timetable_maze' ),  WP_JSON_Server::READABLE )
+            ),
+            '/timetable/mission' => array(
+                array( array( $this, 'get_timetable_mission' ),  WP_JSON_Server::READABLE )
+            ),
+            '/timetable/aztec' => array(
+                array( array( $this, 'get_timetable_aztec' ),  WP_JSON_Server::READABLE )
+            )
+		);
+
+		return array_merge( $routes, $post_routes );
+	}
+
+
+    public function get_timetable_pirates()
+    {
+        // Пираты Карибского моря: Сундук мертвеца
+        $post_id_regs = 492;
+        $id_calendars = 4;
+
+        return $this->get_timetable($post_id_regs, $id_calendars);
+	}
+
+    public function get_timetable_maze()
+    {
+        // Лабиринт
+        $post_id_regs = 495;
+        $id_calendars = 8;
+
+        return $this->get_timetable($post_id_regs, $id_calendars);
+    }
+
+    public function get_timetable_mission()
+    {
+        // Миссия выполнима 2
+        $post_id_regs = 494;
+        $id_calendars = 6;
+
+        return $this->get_timetable($post_id_regs, $id_calendars);
+    }
+
+    public function get_timetable_aztec()
+    {
+        // Сокровища ацтеков
+        $post_id_regs = 1770;
+        $id_calendars = 7;
+
+        return $this->get_timetable($post_id_regs, $id_calendars);
+    }
+
+	/**
+	 * @return stdClass[] Collection of Post entities
+	 */
+	public function get_timetable($post_id_regs, $id_calendars) {
+
+        $wpdb = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
+
+        //$schedule = $wpdb->get_results( "SELECT timm FROM calendars WHERE id=".$id_calendars, ARRAY_A );
+        $price1_time = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE post_id=".$post_id_regs." AND meta_key='kvest_price1_time'", ARRAY_A );
+        $price2_time = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE post_id=".$post_id_regs." AND meta_key='kvest_price2_time'", ARRAY_A );
+        $price3_time = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE post_id=".$post_id_regs." AND meta_key='kvest_price3_time'", ARRAY_A );
+        $room_price1 = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE post_id=".$post_id_regs." AND meta_key='room_price_1'", ARRAY_A );
+        $room_price2 = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE post_id=".$post_id_regs." AND meta_key='room_price_2'", ARRAY_A );
+        $room_price3 = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE post_id=".$post_id_regs." AND meta_key='room_price_3'", ARRAY_A );
+
+        /*--- Schedule from a few price times ---*/
+        //$times_arr  = preg_split("/,\s+/", $schedule[0]['timm']);
+        $price1_times_arr = preg_split("/,\s+/", $price1_time[0]['meta_value']);
+        $price2_times_arr = preg_split("/,\s+/", $price2_time[0]['meta_value']);
+        $price3_times_arr = preg_split("/,\s+/", $price3_time[0]['meta_value']);
+
+        /*--- Creating full schedule from two arrays for weekdays, day-offs ---*/
+        /*--- $w_times and $d_times temporary is not used ---*/
+        $w_times = array();
+        $d_times = array();
+        $all_times = array();
+
+        foreach ($price1_times_arr as &$item) {
+
+            if (preg_match("/\*/", $item)) {
+                $item = str_replace("*", "", $item);
+                $d_times[] = $item;
+            } else {
+                $w_times[] = $item;
+                $all_times[] = $item;
+            }
+        }
+
+        foreach ($price2_times_arr as $key => &$item) {
+
+            if (preg_match("/\*/", $item)) {
+                $item = str_replace("*", "", $item);
+                $d_times[] = $item;
+            } else {
+                $w_times[] = $item;
+            }
+
+            if ($this->canAddNewTime($all_times, $item)) $all_times[] = $item;
+        }
+
+        foreach ($price3_times_arr as $key => &$item) {
+
+            if (preg_match("/\*/", $item)) {
+                $item = str_replace("*", "", $item);
+                $d_times[] = $item;
+            } else {
+                $w_times[] = $item;
+            }
+
+            if ($this->canAddNewTime($all_times, $item)) $all_times[] = $item;
+        }
+
+        // Prices
+        $room_price1 = preg_replace("/\s+грн/", "", $room_price1[0]['meta_value']);
+        $room_price2 = preg_replace("/\s+грн/", "", $room_price2[0]['meta_value']);
+        $room_price3 = preg_replace("/\s+грн/", "", $room_price3[0]['meta_value']);
+
+        /*--- Dates array for two weeks ---*/
+        $days_count = 14;
+        $dates = array();
+        $today = date('Y-m-d');
+        $date = new DateTime($today);
+
+        $dates[] = $today;
+
+        for ($i = 1; $i < $days_count; $i++) {
+            $interval = DateInterval::createFromDateString('1 day');
+            $date->add($interval);
+            $new_date = $date->format('Y-m-d');
+            $dates[] = $new_date;
+        }
+
+        // Forming of final array
+        $overall = array();
+
+        foreach ($dates as $date) {
+
+            foreach ($all_times as $time) {
+
+                $row['date'] = $date;
+                $row['time'] = $time;
+                $row['is_free'] = $this->check_busy_times($date, $time, $post_id_regs);
+
+                // Weekday or day-off
+                $day_type = 'weekday';
+                $day_num = date('N', strtotime($date));
+                if ($day_num > 5) $day_type = 'day-off';
+
+                $row['price'] = $this->get_price($time, $day_type, $price1_times_arr, $price2_times_arr, $price3_times_arr, $room_price1, $room_price2, $room_price3).' грн.';
+
+                $overall[] = $row;
+            }
+        }
+
+        $response = $overall;
+		return $response;
+	}
+
+    /**
+     * Check ability to add non-existing time into time array
+     *
+     * @param array $main_times Existing routes
+     * @return bool
+     */
+    public function canAddNewTime(&$main_times, $compared_time) {
+
+        $timeExists = false;
+
+        foreach ($main_times as $main_time) {
+            if ($main_time == $compared_time) $timeExists = true;
+        }
+
+        return !$timeExists;
+    }
+
+
+    public function get_price($time_request, $day_type, $price1_times_arr, $price2_times_arr, $price3_times_arr, $room_price1, $room_price2, $room_price3) {
+
+        $ultimate_price_id = 1;
+
+        if ($day_type == 'weekday') {
+
+            foreach ($price2_times_arr as $time) {
+                if ($time_request == $time) $ultimate_price_id = 2;
+            }
+
+            foreach ($price1_times_arr as $time) {
+                if ($time_request == $time) $ultimate_price_id = 1;
+            }
+        }
+
+        if ($day_type == 'day-off') {
+
+            foreach ($price1_times_arr as $time) {
+                if ($time_request == $time) $ultimate_price_id = 1;
+            }
+
+            foreach ($price2_times_arr as $time) {
+                if ($time_request == $time) $ultimate_price_id = 2;
+            }
+        }
+
+        foreach ($price3_times_arr as $time) {
+            if ($time_request == $time) $ultimate_price_id = 3;
+        }
+
+        switch($ultimate_price_id) {
+            case 1: return $room_price1;
+                break;
+            case 2: return $room_price2;
+                break;
+            case 3: return $room_price3;
+                break;
+            default: return $room_price1;
+                break;
+        }
+    }
+
+
+    public function check_busy_times($date_request, $time_request, $post_id_regs)
+    {
+        global $wpdb;
+        $is_free = true;
+
+        $response = $wpdb->get_results( "SELECT datt FROM regs WHERE post_id=".$post_id_regs." ORDER BY id DESC LIMIT 100", ARRAY_A );
+
+        foreach ($response as $item) {
+
+            $date_arr = explode(" ", $item['datt']);
+            $date = $date_arr[0];
+            $time = $date_arr[1];
+            $time = substr($time, 0, 5);
+
+            if ($date_request == $date && $time_request == $time) $is_free = false;
+            //echo $date." ".$time."\n";
+        }
+
+        return $is_free;
+	}
+
+}
